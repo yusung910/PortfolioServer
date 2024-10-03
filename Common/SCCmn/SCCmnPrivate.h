8df29e9357655213fc1457b87aa912ea349e77ca
yusung910 <yusung910@nate.com>
2024-10-04 00:23:03 +0900

    First committed
    최초 커밋

---
+++ Common/SCCmn/SCCmnPrivate.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#ifndef SERVER_BUILD
+
+#endif // !SERVER_BUILD
+
+#include <map>
+#include <array>
+
+
+#define NOMINMAX
+
+#if _WIN32
+#include <WinSock2.h>
+#else
+
+#endif // _WIN32
+
