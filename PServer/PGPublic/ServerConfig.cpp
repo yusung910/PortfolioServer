8866302b16b92b010d25b2998c3a6100cabb782f
yusung910 <yusung910@nate.com>
2024-11-15 19:02:16 +0900

    ServerConfig 작업

--- PServer/PGPublic/ServerConfig.cpp
+++ PServer/PGPublic/ServerConfig.cpp
@@ -1,2 +1,179 @@
 #include "PGPPrivate.h"
 #include "ServerConfig.h"
+#include <filesystem>
+#include <fstream>
+#include <json/json.h>
+
+#include "Base64Util.h"
+#include "XORUtil.h"
+
+namespace fs = std::filesystem;
+
+bool ServerConfigData::_LoadConfig(const std::wstring& _confFile, size_t _fileSize)
+{
+    if (true == _confFile.empty())
+        return false;
+
+    if (_fileSize == 0 || _fileSize == (uintmax_t)~0)
+        return false;
+
+    std::ifstream localIS(_confFile);
+    if (false == localIS.is_open())
+        return false;
+
+    std::string localStrBuffer;
+    localStrBuffer.reserve(_fileSize);
+
+    std::string localStrLine;
+    while (getline(localIS, localStrLine))
+        localStrBuffer.append(localStrLine);
+
+    localIS.close();
+
+    Json::CharReaderBuilder localJsonBuilder;
+    auto localReader = localJsonBuilder.newCharReader();
+    JSONCPP_STRING localErr;
+    Json::Value localRoot;
+
+    if (false == localReader->parse(localStrBuffer.c_str(), localStrBuffer.c_str() + _fileSize, &localRoot, &localErr))
+        return false;
+
+
+    //암,복호화
+    XORUtil localXORUtil;
+    Base64Util localBase64;
+
+    //MainListener
+    {
+        /*!
+         *  JSON example)
+         *  {
+         *      "MainListener" : {
+         *          "Name"                  : "",
+         *          "ServerID"              : 10000,
+         *          "BindAddress"           : "0.0.0.0",
+         *          "BindPort"              : 35201,
+         *          "TimeoutMS"             : 3600000,
+         *          "MaxConnection"         : 3000,
+         *          "ControlConnection"     : 1500,
+         *          "SendContextLimitCount" : 500,
+         *          "ServiceType"           : "Dev"
+         *      }
+         *  }
+         */
+
+        const auto& localMainListenerInfo = localRoot["MainListener"];
+
+        //MainListener가 없을 경우 종료
+        if (true == localMainListenerInfo.isNull())
+            return false;
+
+        m_oMainListenInfo.m_sServiceName = localMainListenerInfo.get("Name", "Server").asString();
+        m_oMainListenInfo.m_nServerID = localMainListenerInfo.get("ServerID", 0).asInt();
+        m_oMainListenInfo.m_sBindAddress = localMainListenerInfo.get("BindAddress", "127.0.0.1").asString();
+        m_oMainListenInfo.m_nBindPort = localMainListenerInfo.get("BindPort", 0).asInt();
+        m_oMainListenInfo.m_nTimeoutMS = localMainListenerInfo.get("TimeoutMS", DEFAULT_TIMEOUT_MS).asInt();
+        m_oMainListenInfo.m_nMaxConnection = localMainListenerInfo.get("MaxConnection", DEFAULT_CONNECTION_COUNT).asInt();
+        m_oMainListenInfo.m_nControlConnection = localMainListenerInfo.get("ControlConnection", DEFAULT_CONNECTION_CONTROL_COUNT).asInt();
+        m_oMainListenInfo.m_nSendContextLimitCount = localMainListenerInfo.get("SendContextLimitCount", DEFAULT_SEND_CONTEXT_LIMIT_COUNT).asInt();
+        m_oMainListenInfo.m_sServiceType = localMainListenerInfo.get("ServiceType", "Live").asString();
+
+        if (m_oMainListenInfo.m_sServiceType == "Live")
+        {
+            m_oMainListenInfo.m_bIsLive = true;
+        }
+        else
+        {
+            m_oMainListenInfo.m_bIsLive = false;
+        }
+
+        m_oMainListenInfo.m_nClientVer = localMainListenerInfo.get("ClientVer", 0).asInt();
+
+        if (m_oMainListenInfo.m_sServiceName == "Login")
+            m_oMainListenInfo.m_eServerType = EServer::Login;
+        else if (m_oMainListenInfo.m_sServiceName == "Game")
+            m_oMainListenInfo.m_eServerType = EServer::Game;
+        else if (m_oMainListenInfo.m_sServiceName == "Log")
+            m_oMainListenInfo.m_eServerType = EServer::Log;
+        else if (m_oMainListenInfo.m_sServiceName == "Platform")
+            m_oMainListenInfo.m_eServerType = EServer::Platform;
+        else if (m_oMainListenInfo.m_sServiceName == "Messenger")
+            m_oMainListenInfo.m_eServerType = EServer::Messenger;
+
+        m_oMainListenInfo.m_bIsMaintenanceMode = localMainListenerInfo.get("MaintenanceMode", false).asBool();
+    }
+
+    //GameServer List
+    m_umGameServers.clear();
+    /*!
+     *  JsonExample) Root 아래에 넣어야한다.
+     *  {
+     *      GameServerList:{
+     *          "10101" : {
+     *              "ServerGroupID": 1,
+     *              "ServerID": 10101,
+     *              "BindAddress": "0.0.0.0",
+     *              "BindPort": 35202,
+     *              "PublicHost": "",
+     *              "PublicPort": ""
+     *          }
+     *      }
+     *  }
+     */
+    if (const auto& localSubListen = localRoot["GameServerList"]; false == localSubListen.isNull())
+    {
+        for (const auto& localSubName : localSubListen.getMemberNames())
+        {
+            const auto& localSubListenInfo = localSubListen[localSubName];
+
+            if (true == localSubListenInfo.isNull())
+                continue;
+
+            ServerListenerInfo insertSubListenerInfo;
+            insertSubListenerInfo.m_nServerGroupID = localSubListenInfo.get("ServerGroupID", 0).asInt();
+            insertSubListenerInfo.m_nServerID = localSubListenInfo.get("ServerID", 0).asInt();
+            insertSubListenerInfo.m_sBindAddress = localSubListenInfo.get("BindAddress", 0).asString();
+            insertSubListenerInfo.m_nBindPort = localSubListenInfo.get("BindPort", 0).asInt();
+
+            insertSubListenerInfo.m_sPublicHost = localSubListenInfo.get("PublicHost", 0).asString();
+            insertSubListenerInfo.m_nPublicPort = localSubListenInfo.get("PublicPort", 0).asInt();
+
+            if (insertSubListenerInfo.m_sPublicHost.empty() || insertSubListenerInfo.m_nPublicPort == 0)
+            {
+                std::cout << "Wrong Server Config. Check 'GameServerList' Info" << std::endl;
+                continue;
+            }
+
+            m_umGameServers.insert(std::pair(localSubName, insertSubListenerInfo));
+            m_umServerTypeByServerID.insert_or_assign(insertSubListenerInfo.m_nServerID, EServer::Game);
+        }
+    }
+
+    return true;
+}
+
+void ServerConfigData::_Clear()
+{
+    m_umSubListenInfo.clear();
+    m_umConnectorInfo.clear();
+    m_umDBConnectionInfo.clear();
+    m_umADBConnectionInfo.clear();
+    m_umGDBConnectionInfo.clear();
+    m_umEDBConnectionInfo.clear();
+    m_umLDBConnectionInfo.clear();
+
+    m_umObjectPoolInfo.clear();
+
+    m_bIsLoaded = false;
+}
+
+void ServerConfigData::_LoadCPUCount()
+{
+    SYSTEM_INFO localInfo;
+    GetSystemInfo(&localInfo);
+
+    m_nProcessCount = (int)localInfo.dwNumberOfProcessors;
+
+    if (m_nProcessCount <= 0)
+        m_nProcessCount = 0;
+}
--- PServer/PGPublic/ServerConfig.h
+++ PServer/PGPublic/ServerConfig.h
@@ -1,5 +1,167 @@
+/**
+ *  @file ServerConfig.h
+ *  @author YS
+ *  @date 2024-11-15
+ *  @project PGPublic
+ *
+ *  서버 실행에 필요한 클래스가 모여있다
+ */
 #pragma once
-class ServerConfig
+#include <string>
+#include <unordered_map>
+#include <RefSingleton.h>
+#include "ELang.h"
+
+#include <ServerConfigInfo.h>
+#include <GlobalEnum.h>
+
+constexpr int DEFAULT_CONNECTION_COUNT = 7000;
+constexpr int DEFAULT_CONNECTION_CONTROL_COUNT = 6000;
+constexpr int DEFAULT_SEND_CONTEXT_LIMIT_COUNT = 500;
+
+
+/*!
+ *  서버 실행시 Listener 정보를 관리하는 클래스
+ */
+class ServerListenerInfo : public ListenerInfo
 {
+public:
+    //서버 그룹 아이디
+    int m_nServerGroupID = 0;
+    //서버 식별 아이디
+    int m_nServerID = 0;
+    //접속 통제가 필요한 시점의 기준 인원(접속 속도를 저하시키는 시점)
+    int m_nControlConnection = DEFAULT_CONNECTION_CONTROL_COUNT;
+    //서버 접속한 클라이언트에게 보내는 Context수가 이
+    int m_nSendContextLimitCount = DEFAULT_SEND_CONTEXT_LIMIT_COUNT;
+
+
+    std::string m_sServiceType = "";
+
+    //클라이언트에게 알릴 접속 정보
+    std::string m_sPublicHost = "";
+    int m_nPublicPort = 0;
+    int m_nClientVer = 0;
+
+    bool m_bIsLive = false;
+
+    EServer::Type m_eServerType = EServer::None;
+
+    bool m_bIsMaintenanceMode = false; //점검 중 라이브 빌드 접속 가능 여부를 확인
+
 };
 
+using ConnectorInfo = ServerListenerInfo;
+
+constexpr int CONFIG_SWITCH_SIZE = 2;
+
+class ServerConfigData
+{
+private:
+    //Listener
+    ServerListenerInfo m_oMainListenInfo;
+    std::unordered_map<std::string, ServerListenerInfo> m_umSubListenInfo;
+
+    //Connector
+    std::unordered_map<std::string, ConnectorInfo> m_umConnectorInfo;
+    
+    //-------------------------------------------------------------------
+    //DB
+    //-------------------------------------------------------------------
+    std::unordered_map<std::string, DBInfo> m_umDBConnectionInfo;
+    //AccountDB
+    std::unordered_map<int, DBInfo> m_umADBConnectionInfo;
+    //GameDB
+    std::unordered_map<int, DBInfo> m_umGDBConnectionInfo;
+    //ExchangeDB
+    std::unordered_map<int, DBInfo> m_umEDBConnectionInfo;
+    //LogDB
+    std::unordered_map<int, DBInfo> m_umLDBConnectionInfo;
+    //-------------------------------------------------------------------
+
+
+    //Log
+    std::string m_sLogDir = "";
+    int m_nLogLvl = 0;
+
+    //Dump
+    std::string m_sDumpDir = "";
+
+    //ObjectPool
+    std::unordered_map<std::string, int> m_umObjectPoolInfo;
+
+    //Map Info
+    std::string m_sMapInfoDir = "";
+
+    //
+    bool m_bProtocolCheck = false;
+
+    bool m_bTracePacket = false;
+    bool m_bTracePacketIgnoreBattle = false;
+
+    //MDB
+    bool m_bUseMDB = false;
+    std::string m_sMDBPath = "";
+
+    bool m_bIsLoaded = false;
+
+    //ProcessCount
+    int m_nProcessCount = 1;
+
+    //ServerList
+    std::unordered_map<std::string, ConnectorInfo> m_umGameServers;
+    std::unordered_map<std::string, ConnectorInfo> m_umLoginServers;
+    std::unordered_map<std::string, ConnectorInfo> m_umMessengerServers;
+    std::unordered_map<std::string, ConnectorInfo> m_umLogServers;
+
+
+    std::string m_sPaymentURL;
+    std::string m_sAPIServerURL;
+
+    std::unordered_map<int, EServer::Type> m_umServerTypeByServerID;
+
+    //Platform
+    std::string m_sGoogleClientID = "";
+    std::string m_sGooglePrivateFileName = "";
+    std::string m_sGooglePackageName = "";
+    std::string m_sGoogleAPIKey = "";
+
+    std::string m_sOneStoreID = "";
+    std::string m_sOneStoreClientID = "";
+    std::string m_sOneStoreKey = "";
+
+    std::wstring m_sConfigFileName = L"";
+
+    ELang m_eLanguage = ELang::KR;
+
+public:
+
+private:
+
+    /*!
+     *  json파일에 저장된 serverconfig 내용을 Load하고
+     *  사용 할 수 있도록 변수에 파싱하는 함수
+     *
+     *      @param [in] _confFile 
+     *      @param [in] _fileSize 
+     *
+     *      @return 
+     */
+    bool _LoadConfig(const std::wstring& _confFile, size_t _fileSize);
+
+    /*!
+     *  Connection 관련 unordered_map 변수 초기화
+     */
+    void _Clear();
+
+    /*!
+     *  서버 컴퓨터의 CPU 코어 갯수를 호출 GetSystemInfo()
+     */
+    void _LoadCPUCount();
+};
+
+class ServerConfig
+{
+private:
+    ServerConfigData m_oServerConfigData[CONFIG_SWITCH_SIZE];
+};
