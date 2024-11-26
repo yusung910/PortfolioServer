800ac5d7260b6f07de17ea809b201d5bc6fe63e7
yusung910 <yusung910@nate.com>
2024-11-26 19:25:57 +0900

    bot 클라 작업

--- PServer/BotClient/Network/NetworkManager.cs
+++ PServer/BotClient/Network/NetworkManager.cs
@@ -1,4 +1,5 @@
 ﻿using FlatBuffers;
+using lz4;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,7 +19,8 @@ namespace BotClient.Network
         private static int MAX_PACKET_BINARY_SIZE = 8192 * 2;              // 패킷 바이너리 최대 크기 (Payload)
         private static int PACKET_HEADER_SIZE = 8;                 // size 4 + protocol 4
         private static int MAX_PACKET_DATA_SIZE = MAX_PACKET_BINARY_SIZE - PACKET_HEADER_SIZE;	// 패킷에 들어갈수 있는 최대 데이터 크기
-
+        private static uint PACKET_COMPRESS_MASK = 0x80000000;
+        private static int DEFAULT_PACKET_COMPRESS_START_SIZE = 60;
         bool m_IsConnected = false;
 
         Socket m_oSocket;
@@ -69,9 +71,12 @@ namespace BotClient.Network
         {
             if (_args.BytesTransferred > 0 && _args.SocketError == SocketError.Success)
             {
+                //패킷 압축 여부에 따라 lz4를 이용해서 압축, 해제 한다
+                //https://github.com/IonKiwi/lz4.net
                 //buff.Get(0~3)->패킷 사이즈
                 //buff.Get(4~7)->MessageID
                 //others -> 데이터
+                Byte firstByte = _args.Buffer[0];
                 ByteBuffer buff = new ByteBuffer(_args.Buffer);
                 //패킷 사이즈
                 int payLoadSize = 0;
@@ -109,8 +114,8 @@ namespace BotClient.Network
 
             //Flatbuffer 데이터 빌드 할 때 각각 패킷에 들어갈 데이터 먼저 빌드 해줘야한다(CreateString 등등을 먼저 해야한다)
             FlatBufferBuilder builder = new FlatBufferBuilder(1);
-            var id = builder.CreateString("Test");
-            var pw = builder.CreateString("1234");
+            var id = builder.CreateString("yusung910@nate.com");
+            var pw = builder.CreateString("1234123AB!!@");
 
             CSAuthReq.StartCSAuthReq(builder);
 
@@ -120,11 +125,27 @@ namespace BotClient.Network
             var endoffset = CSAuthReq.EndCSAuthReq(builder);
             
             builder.Finish(endoffset.Value);
+            //flatbuffer byte
             var tmp = builder.SizedByteArray();
 
-            int payloadSize = builder.SizedByteArray().Length + PACKET_HEADER_SIZE;
-            byte[] payloadSizeByte = BitConverter.GetBytes(payloadSize);
+            //body 사이즈
+            int packetBody = builder.SizedByteArray().Length;
 
+            //패킷 총 크기
+            int payloadSize = packetBody + PACKET_HEADER_SIZE;
+
+            //패킷 payload byte 배열
+            byte[] payloadSizeByte = new byte[4];
+
+            //패킷 body 사이즈에 따라 압축 여부 지정
+            if (packetBody > DEFAULT_PACKET_COMPRESS_START_SIZE)
+            {
+                payloadSizeByte = BitConverter.GetBytes(payloadSize | PACKET_COMPRESS_MASK);
+            }
+            else
+            {
+                payloadSizeByte = BitConverter.GetBytes(payloadSize & ~PACKET_COMPRESS_MASK);
+            }
 
             int tmpMsgID = (int)EPacketProtocol.CS_AuthReq;
             byte[] msgIDBytes = new byte[4];
@@ -139,9 +160,13 @@ namespace BotClient.Network
 
             Array.Copy(msgIDBytes, 0, packet, 4, msgIDBytes.Length);
 
-
             Array.Copy(tmp, 0, packet, PACKET_HEADER_SIZE, tmp.Length);
 
+            if (packetBody > DEFAULT_PACKET_COMPRESS_START_SIZE)
+            {
+                //패킷 암호화
+                packet = LZ4Helper.Compress(packet);
+            }
 
             m_oSendSAArgs.SetBuffer(packet, 0, packet.Length);
             
