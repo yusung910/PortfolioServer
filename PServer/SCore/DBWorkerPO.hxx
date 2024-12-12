8c181ba5365364ec85526c593126bfa936ef8a1e
yusung910 <yusung910@nate.com>
2024-12-13 01:12:05 +0900

    poco db lib 세팅

--- PServer/SCore/DBWorkerPO.hxx
+++ PServer/SCore/DBWorkerPO.hxx
@@ -10,8 +10,33 @@
 #pragma warning(disable : 4996)
 #include <string>
 #include <atomic>
-#include <atldbcli.h>  
-#include "DBSession.hxx"
+ // Poco Include 관련
+#ifndef POCO_STATIC
+#	define POCO_STATIC 1
+#endif
+
+#pragma warning (push)
+#pragma warning (disable: 4244 4245 4100 4127)
+#pragma warning (disable: 4634 4635)	// XML 주석 관련
+#pragma warning (disable: 4389)			// Visual Studio 2022 (vc 143) , signed, unsigned 불일치
+#pragma warning (disable: 26439 26498 26451 26444 26451 26812)	// VC++ Code Guidelines warning. POCO 라이브러리가 문제...
+
+#include <Poco/DateTime.h>
+#include <Poco/Data/RecordSet.h>
+#include <Poco/Data/ODBC/ODBCException.h>
+#include <Poco/Data/ODBC/ODBCStatementImpl.h>
+
+#pragma warning (pop)
+
+#pragma comment (lib, "iphlpapi.lib")	// GetAdaptersInfo()
+
+namespace Poco::Data
+{
+	class Session;
+}
+
+using namespace Poco::Data::Keywords;		// in() out()
+
 
 constexpr size_t PROPERTY_MAX_FIELD_SIZE = 8192;
 constexpr size_t DEFAULT_DATABASE_TIMEOUT = 5;		// 단위는 확인 필요
